750b0702cfcdf08b97ae8595d487f5512251be8c
Daiki Oshima <daikougun0923@gmail.com>
2023-02-15 15:46:13 +0900

    change : footer font

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="sticky top-full">
+    <footer className="sticky top-full font-zenkaku font-light">
       <small className="block text-center text-accent leading-10">
         &copy;2022 enFiler - Daiki Oshima
       </small>
